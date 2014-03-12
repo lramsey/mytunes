02c7498131bfab685787f2e5b2ed940e5c68b108
Hack Reactor <hackreactor@HR-A4.local>
2014-03-12 16:18:09 -0700

    Set up enqueue and updated app view and song queue view

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -2,17 +2,28 @@
 var AppView = Backbone.View.extend({
 
   initialize: function(params){
+    //params are appmodel   this.model = appModel
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
+    //passing Songs collection to libraryView through appmodel
     this.libraryView = new LibraryView({collection: this.model.get('library')});
+    this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     this.model.on('change:currentSong', function(model){
       this.playerView.setSong(model.get('currentSong'));
+    }, this);
+
+    this.model.on('change:songQueue', function(model){
+      var sq = model.get('songQueue');
+      var song = sq.models[sq.models.length-1];
+      console.log('Song', song);
+      this.songQueueView.addSong(song);
     }, this);
   },
 
   render: function(){
     return this.$el.html([
       this.playerView.$el,
+      this.songQueueView.$el,
       this.libraryView.$el
     ]);
   }
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,44 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.render();
   },
 
   render: function() {
+    this.$el.html('<th>Playlist</th>');
     return this.$el;
+  },
+
+  addSong: function(song) {
+    this.$el.append( new SongQueueEntryView({model: song}).render() );
   }
 
 });
+
+/*
+var LibraryView = Backbone.View.extend({
+
+  tagName: "table",
+
+  initialize: function() {
+    this.render();
+  },
+
+  render: function(){
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
+
+    this.$el.html('<th>Library</th>').append(
+      //library view has songs collection
+      this.collection.map(function(song){
+        return new LibraryEntryView({model: song}).render();
+      })
+    );
+  }
+
+});
+*/

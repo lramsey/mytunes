6210a9185ae79223389e73e338f74486f5368cc4
lramsey <lramsey177@gmail.com>
2014-03-13 20:52:19 -0700

    scratch work

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,19 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+  initialize: function(song){
+    _.bindAll(this, play, render);
+    this.song = song;
+    this.render();
+  },
+
+  play: function(){
+    //if song is last in collection, next in line as it were
+    if(this.songQueue[0] === this.song){
+      this.setSong(this.song);
+    }
+  },
+  render: function(){
+
+  }
 });
